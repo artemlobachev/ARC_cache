8a5546b6d622bd4b297ad8115a48e831abee18ac
artemlobachev <lobachev.av@phystech.edu>
2025-09-15 15:31:34 +0300

    well done

--- include/ARC_Cache.hpp
+++ include/ARC_Cache.hpp
@@ -43,38 +43,40 @@ private:
         LocationInfo(ListLocation loc, ListIter it) : location(loc), list_iter(it) {}    
     };
 
-    std::size_t capacity;
-    std::size_t hits_counter;
-    double adapt_param;
+    std::size_t capacity_;
+    std::size_t hits_counter_;
+    double adapt_param_;
     
-    ListType list_first,       list_frequent;
-    ListType list_first_ghost, list_frequent_ghost; 
+    ListType list_first_,       list_frequent_;
+    ListType list_first_ghost_, list_frequent_ghost_; 
 
-    using HashmapType = typename std::unordered_map<key_t, LocationInfo>;
-    using HashmapIter = typename HashmapType::iterator;
+    using CacheMapType = typename std::unordered_map<key_t, LocationInfo>;
+    using CacheMapIter = typename CacheMapType::iterator;
 
-    HashmapType hashmap;
+    CacheMapType cache_map_;
 
     const std::size_t STD_CAPACITY = 64;
 
     void adapt_first_ghost()
     {
-        assert(list_first_ghost.size());
+        assert(list_first_ghost_.size());
 
-        double ratio_between_ghosts_size = static_cast<double>(list_frequent_ghost.size()) / list_first_ghost.size();
+        double ratio_between_ghosts_size = static_cast<double>(list_frequent_ghost_.size()) / 
+                                           static_cast<double>(list_first_ghost_.size());
         double max_ = std::max(1.0, ratio_between_ghosts_size);
 
-        adapt_param = std::min(adapt_param + max_, static_cast<double>(capacity));
+        adapt_param_ = std::min(adapt_param_ + max_, static_cast<double>(capacity_));
     }
 
     void adapt_second_ghost() 
     {
-        assert(list_frequent_ghost.size());
+        assert(list_frequent_ghost_.size());
         
-        const double ratio_between_ghosts_size = static_cast<double>(list_first_ghost.size()) / list_frequent_ghost.size();
+        const double ratio_between_ghosts_size = static_cast<double>(list_first_ghost_.size()) / 
+                                                 static_cast<double>(list_frequent_ghost_.size());
         const double max_ = std::max(1.0, ratio_between_ghosts_size);
 
-        adapt_param = std::max(adapt_param - max_, 0.0);
+        adapt_param_ = std::max(adapt_param_ - max_, 0.0);
     }
 
     inline void print_header(const std::string &word) const
@@ -115,35 +117,35 @@ private:
 
     inline bool not_empty_and_adaptive() const
     {
-        const std::size_t list_size_tmp = list_first.size(); // size1
+        const std::size_t list_size_tmp = list_first_.size(); // size1
 
         bool is_list_not_empty = list_size_tmp >= 1;
-        bool is_list_adaptive_enough = list_size_tmp > static_cast<std::size_t>(adapt_param) || 
-                                        (list_size_tmp == static_cast<std::size_t>(adapt_param) && !list_frequent_ghost.empty());
+        bool is_list_adaptive_enough = list_size_tmp > static_cast<std::size_t>(adapt_param_) || 
+                                        (list_size_tmp == static_cast<std::size_t>(adapt_param_) && !list_frequent_ghost_.empty());
 
         return is_list_not_empty && is_list_adaptive_enough;
     }
 
     inline void replace_first_and_ghost()
     {
-        assert(!list_first.empty());
+        assert(!list_first_.empty());
 
-        const CacheEntry tail_element = list_first.back();
-        list_first.pop_back();
+        const CacheEntry tail_element = list_first_.back();
+        list_first_.pop_back();
 
-        list_first_ghost.push_front(tail_element);
-        update_hashmap(tail_element.key, ListLocation::FIRST_LIST_GHOST, list_first_ghost.begin());
+        list_first_ghost_.push_front(tail_element);
+        update_cache_map(tail_element.key, ListLocation::FIRST_LIST_GHOST, list_first_ghost_.begin());
     }
 
     inline void replace_frequent_and_ghost()
     {   
-        assert(!list_frequent.empty());
+        assert(!list_frequent_.empty());
         
-        const CacheEntry tail_element = list_frequent.back();
-        list_frequent.pop_back();
+        const CacheEntry tail_element = list_frequent_.back();
+        list_frequent_.pop_back();
 
-        list_frequent_ghost.push_front(tail_element);
-        update_hashmap(tail_element.key, ListLocation::FREQUENT_LIST_GHOST, list_frequent_ghost.begin());
+        list_frequent_ghost_.push_front(tail_element);
+        update_cache_map(tail_element.key, ListLocation::FREQUENT_LIST_GHOST, list_frequent_ghost_.begin());
     }
 
     void replace_for_adapt()
@@ -152,50 +154,50 @@ private:
         else                          replace_frequent_and_ghost();
     }
 
-    inline void update_hashmap(const key_t &key, ListLocation location, ListIter list_it)
+    inline void update_cache_map(const key_t &key, ListLocation location, ListIter list_it)
     {
-        hashmap[key] = LocationInfo(location, list_it);
+        cache_map_[key] = LocationInfo(location, list_it);
     }
 
-    bool handle_existing_item(const HashmapIter &hashmap_it, const key_t &key)
+    bool handle_existing_item(const CacheMapIter &cache_map_it, const key_t &key)
     {  
         //  dump();
-        ListLocation location  = hashmap_it->second.location;
-        ListIter list_iterator = hashmap_it->second.list_iter;
+        ListLocation location  = cache_map_it->second.location;
+        ListIter list_iterator = cache_map_it->second.list_iter;
 
         switch(location)
         {
             case ListLocation::FIRST_LIST:
-                // hashmap_dump(hashmap);
-                list_frequent.push_front(*list_iterator);
-                list_first.erase(list_iterator);
+                // cache_map_dump(cache_map);
+                list_frequent_.push_front(*list_iterator);
+                list_first_.erase(list_iterator);
 
-                update_hashmap(key, ListLocation::FREQUENT_LIST, list_frequent.begin());                    
-                hits_counter++;
+                update_cache_map(key, ListLocation::FREQUENT_LIST, list_frequent_.begin());                    
+                hits_counter_++;
                 return true;
 
             case ListLocation::FREQUENT_LIST: 
-                hits_counter++;
+                hits_counter_++;
                 return true;
 
             case ListLocation::FIRST_LIST_GHOST:
                 adapt_first_ghost();
                 replace_for_adapt();
 
-                list_frequent.push_front(*list_iterator);
-                list_first_ghost.erase(list_iterator);
+                list_frequent_.push_front(*list_iterator);
+                list_first_ghost_.erase(list_iterator);
 
-                update_hashmap(key, ListLocation::FREQUENT_LIST, list_frequent.begin());
+                update_cache_map(key, ListLocation::FREQUENT_LIST, list_frequent_.begin());
                 return false;
 
             case ListLocation::FREQUENT_LIST_GHOST:
                 adapt_second_ghost();
                 replace_for_adapt();
 
-                list_frequent.push_front(*list_iterator);
-                list_frequent_ghost.erase(list_iterator);
+                list_frequent_.push_front(*list_iterator);
+                list_frequent_ghost_.erase(list_iterator);
 
-                update_hashmap(key, ListLocation::FREQUENT_LIST, list_frequent.begin());
+                update_cache_map(key, ListLocation::FREQUENT_LIST, list_frequent_.begin());
                 return false;
             
             case ListLocation::NOT_FOUND:
@@ -208,40 +210,40 @@ private:
 
     inline void handle_cache_overflow()
     {
-        std::size_t list1_size   = list_first.size();
-        std::size_t list1gh_size = list_first_ghost.size();
-        std::size_t list2_size   = list_frequent.size();
-        std::size_t list2gh_size = list_frequent_ghost.size();
+        std::size_t list1_size   = list_first_.size();
+        std::size_t list1gh_size = list_first_ghost_.size();
+        std::size_t list2_size   = list_frequent_.size();
+        std::size_t list2gh_size = list_frequent_ghost_.size();
 
         std::size_t sum_size_lists = list1_size + list2_size + list1gh_size + list2gh_size;
-        if (list1_size + list1gh_size == capacity)
+        if (list1_size + list1gh_size == capacity_)
         {
-            if (list1_size < capacity)
+            if (list1_size < capacity_)
             {
-                if (!list_first_ghost.empty())
+                if (!list_first_ghost_.empty())
                 {
-                    hashmap.erase(list_first_ghost.back().key);
-                    list_first_ghost.pop_back();
+                    cache_map_.erase(list_first_ghost_.back().key);
+                    list_first_ghost_.pop_back();
                 }
                 replace_for_adapt();
             }
             else
             {
-                if (!hashmap.empty())
+                if (!cache_map_.empty())
                 {
-                    hashmap.erase(list_first.back().key);
-                    list_first.pop_back();
+                    cache_map_.erase(list_first_.back().key);
+                    list_first_.pop_back();
                 }
             }
         }
-        else if (list1_size + list1gh_size < capacity && sum_size_lists >= capacity)      
+        else if (list1_size + list1gh_size < capacity_ && sum_size_lists >= capacity_)      
         {    
-            if (sum_size_lists == 2 * capacity)
+            if (sum_size_lists == 2 * capacity_)
             { 
-                if (!list_frequent_ghost.empty())
+                if (!list_frequent_ghost_.empty())
                 {    
-                    hashmap.erase(list_frequent_ghost.back().key);
-                    list_frequent_ghost.pop_back();       
+                    cache_map_.erase(list_frequent_ghost_.back().key);
+                    list_frequent_ghost_.pop_back();       
                 }
             }
             replace_for_adapt();
@@ -252,16 +254,16 @@ private:
     {
         handle_cache_overflow();
 
-        list_first.push_front(CacheEntry(key, item));
-        update_hashmap(key, ListLocation::FIRST_LIST, list_first.begin());
+        list_first_.push_front(CacheEntry(key, item));
+        update_cache_map(key, ListLocation::FIRST_LIST, list_first_.begin());
     }
 
 
-    void hashmap_dump(const HashmapType &hashmap) const
+    void cache_map_dump(const CacheMapType &cache_map) const
     {
-        print_header("HASHMAP DUMP");
+        print_header("cache_map DUMP");
 
-        for (const auto &pair : hashmap) // pair = hashmap.begin -> hashmap.end() <=> pair - hashmap_it
+        for (const auto &pair : cache_map) // pair = cache_map.begin -> cache_map.end() <=> pair - cache_map_it
         {
             const LocationInfo &loc_info = pair.second; // pair.second - iterator 
             const ListIter     &list_it  = loc_info.list_iter;
@@ -321,7 +323,7 @@ private:
             item_t item = cache.item;
             key_t  key  = cache.key;
 
-            std::cout << "[ item" << i++ <<" = " << item <<"(hashmap key: " << key <<  ") ]";
+            std::cout << "[ item" << i++ <<" = " << item <<"(cache_map key: " << key <<  ") ]";
             if (i < size) std::cout << "<==>";
         }
 
@@ -330,9 +332,9 @@ private:
     }
 
 public:
-    ARCCache() : capacity(0), hits_counter(0), adapt_param(0.0)  {}
+    ARCCache() : capacity_(0), hits_counter_(0), adapt_param_(0.0)  {}
 
-    ARCCache(std::size_t capacity) : capacity(capacity), hits_counter(0), adapt_param(0.0) {
+    ARCCache(std::size_t capacity) : capacity_(capacity), hits_counter_(0), adapt_param_(0.0) {
         if (capacity == 0)
         {
             std::cout << "Capacity must be greater then 0\n capacity = STD_CAPACITY = " << STD_CAPACITY << std::endl;
@@ -342,21 +344,21 @@ public:
 
     item_t get_item(const key_t &key) const
     {
-        HashmapIter hashmap_it = hashmap.find(key);
+        CacheMapIter cache_map_it = cache_map_.find(key);
         
-            if (hashmap_it == hashmap.end()) return item_t();
+            if (cache_map_it == cache_map_.end()) return item_t();
 
-        return hashmap_it->second.list_iter->item;
+        return cache_map_it->second.list_iter->item;
     }
 
     bool add_cache(const key_t &key, const item_t &item)
     {
-        assert(capacity > 0);
+        if (capacity_ <= 0) return false;
         
-        HashmapIter hashmap_it = hashmap.find(key); 
+        CacheMapIter cache_map_it = cache_map_.find(key); 
 
-        if (hashmap_it != hashmap.end()) 
-            return handle_existing_item(hashmap_it, key);
+        if (cache_map_it != cache_map_.end()) 
+            return handle_existing_item(cache_map_it, key);
         else                           
         { 
             add_new_item(key, item); 
@@ -374,30 +376,30 @@ public:
 
     ~ARCCache()
     {
-        list_first.clear();
-        list_frequent.clear();
-        list_first_ghost.clear();
-        list_frequent_ghost.clear();
+        list_first_.clear();
+        list_frequent_.clear();
+        list_first_ghost_.clear();
+        list_frequent_ghost_.clear();
 
-        hashmap.clear();
+        cache_map_.clear();
     }
 
-    std::size_t get_hit_count() const { return hits_counter; }
+    std::size_t get_hit_count() const { return hits_counter_; }
 
     void dump() const
     {
         print_header("Adaptive replacement cache DUMP");
         
-        std::cout << "capacity: " << capacity << std::endl;
-        std::cout << "hit count: " << hits_counter << std::endl;
-        std::cout << "adadptive parametr:" << static_cast<double>(adapt_param) << std::endl;
+        std::cout << "capacity: " << capacity_ << std::endl;
+        std::cout << "hit count: " << hits_counter_ << std::endl;
+        std::cout << "adadptive parametr:" << static_cast<double>(adapt_param_) << std::endl;
 
-        hashmap_dump(hashmap);
+        cache_map_dump(cache_map_);
 
-        list_dump(list_first, ListLocation::FIRST_LIST);
-        list_dump(list_frequent, ListLocation::FREQUENT_LIST);
-        list_dump(list_first_ghost, ListLocation::FIRST_LIST_GHOST);
-        list_dump(list_frequent_ghost, ListLocation::FREQUENT_LIST_GHOST);
+        list_dump(list_first_, ListLocation::FIRST_LIST);
+        list_dump(list_frequent_, ListLocation::FREQUENT_LIST);
+        list_dump(list_first_ghost_, ListLocation::FIRST_LIST_GHOST);
+        list_dump(list_frequent_ghost_, ListLocation::FREQUENT_LIST_GHOST);
 
         print_separator();
     }
--- include/optimal_cache.hpp
+++ include/optimal_cache.hpp
@@ -1,35 +1,39 @@
+#ifndef OPTIMAL_CACHE_HPP 
+#define OPTIMAL_CACHE_HPP
+
 #include <unordered_map>
 #include <deque>
 #include <vector>
-
+#include <iostream>
+#include <cstdint>
 
 template <typename key_t, typename item_t>
 struct OPT_cache
 {
 private:
-    std::size_t capacity;
-    std::size_t amount_of_items;
-    std::size_t hits_counter;
-
-    using request_t = typename std::pair<key_t, item_t>;
     using index_t   = typename std::size_t;
-
+    using request_t = typename std::pair<key_t, item_t>;
     using IndexMapType = typename std::unordered_map<key_t, std::deque<index_t>>;
-    IndexMapType map_future;
-
     using CacheMapType = typename std::unordered_map<key_t, item_t>;
-    CacheMapType cache_map;
-
     using input_vector = typename std::vector<request_t>;
-    input_vector items = 0;
+
+    static constexpr key_t POISON_KEY = static_cast<std::size_t>(0XEDAA);
+    static constexpr index_t INF_INDEX = UINT64_MAX;
+
+
+    std::size_t capacity_;
+    std::size_t amount_of_items_;
+    std::size_t hits_counter_;
+
+    IndexMapType map_future_;
+    CacheMapType cache_map_;
+
 
 public:
-    OPT_cache() : capacity(0), amount_of_items(0), hits_counter(0), items(0) {}
-    OPT_cache(std::size_t input_capacity, std::size_t input_amount_of_items, const input_vector &input_items) 
-             : capacity(input_capacity), amount_of_items(input_amount_of_items), hits_counter(0), items(input_items)
+    OPT_cache() : capacity_(0), amount_of_items_(0), hits_counter_(0) {}
+    OPT_cache(std::size_t input_capacity, std::size_t input_amount_of_items) 
+             : capacity_(input_capacity), amount_of_items_(input_amount_of_items), hits_counter_(0)
     {
-        items.reserve(input_amount_of_items);  
-        load_map_of_future();  
     }
     
 /*    bool add_cache(const key_t &key, const item_t &item)
@@ -38,71 +42,93 @@ public:
 
     } */
 
-    void run_optimal_cache()
+
+    void run_optimal_cache(const input_vector &key_items)
     {
-        for (std::size_t i = 0; i < items.size(); i++)
+        if (capacity_ == 0) return;
+        
+        load_map_of_future(key_items);
+        for (std::size_t i = 0; i < key_items.size(); i++)
         {
-            key_t  curr_key  = items[i].first;
-            item_t curr_item = items[i].second;
+            const auto &[curr_key, curr_item] = key_items[i];
+ 
+            if (!map_future_[curr_key].empty())
+                map_future_[curr_key].pop_front();
 
-            if (cache_map.find(curr_key) != cache_map.end())
-            { 
-                hits_counter++;
-            }
+            if (cache_map_.find(curr_key) != cache_map_.end())
+                hits_counter_++;
 
-            else if (cache_map.size() < capacity)
-                    cache_map[curr_key] = curr_item;
-            else    
+            else if (cache_map_.size() < capacity_)
+                cache_map_[curr_key] = curr_item;
+
+            else
             {
-                remove_farest();
-                cache_map[curr_key] = curr_item;
+                bool should_be_replace = remove_farest(curr_key);
+                if (should_be_replace) cache_map_[curr_key] = curr_item;
             }
         }
     }
 
-    std::size_t get_hit_count() const
+    inline std::size_t get_hit_count() const
     {
-        return hits_counter;
+        return hits_counter_;
     }
 
     ~OPT_cache() 
     {
-        map_future.clear();
+        map_future_.clear();
     }
 
 private:
-    void remove_farest()
+    bool remove_farest(const key_t &insert_key)
     {
-        index_t farthest_next_id = -1;
-        key_t   farthest_key  = -1;
-
-        for (const auto &pair : cache_map)
+        index_t farthest_next_index = 0;
+        key_t   farthest_key  = POISON_KEY;
+        const index_t next_insert_index = (map_future_[insert_key].empty()) ?  
+                                           INF_INDEX : map_future_[insert_key].front();
+        
+        for (const auto &[key_in_cache, item] : cache_map_)
         {
-            key_t key_in_cache = pair.first;
-            index_t closest_index = map_future[key_in_cache].front();
-            
-            if (map_future[key_in_cache].empty())
+            if (map_future_[key_in_cache].empty())
             {
                 farthest_key = key_in_cache;
                 break;
             }
-            else if (closest_index > farthest_next_id)
+            else 
             {
-                farthest_next_id = closest_index;
-                farthest_key  = key_in_cache;
+                index_t next_index = map_future_[key_in_cache].front();
+                if (next_index > farthest_next_index)
+                {
+                        farthest_next_index = next_index;
+                        farthest_key        = key_in_cache;
+                }
             }
         }
-
-        cache_map.erase(farthest_key);
-      //  map_future[farthest_key].pop_front();
+        if (farthest_next_index > next_insert_index && farthest_key != POISON_KEY)
+        {
+            cache_map_.erase(farthest_key);
+            return true;
+        }
+        else 
+            return false;
     }
 
-    void load_map_of_future()
+    inline void cache_map_dump() const
     {
-        for (index_t i = 0; i < items.size(); i++)
+        for (const auto &pair : cache_map_)
         {
-            key_t key = items[i].first;
-            map_future[key].push_back(i);        
+            key_t key_in_cache = pair.first;
+            item_t item_in_cache = pair.second;
+
+            std::cout << "key: " << key_in_cache << " item: " << item_in_cache << std::endl;
         }
     }
+
+    void load_map_of_future(const input_vector &key_items)
+    {
+        for (index_t i = 0; i < key_items.size(); i++)
+            map_future_[key_items[i].first].push_back(i + 1);        
+    }
 };
+
+#endif
